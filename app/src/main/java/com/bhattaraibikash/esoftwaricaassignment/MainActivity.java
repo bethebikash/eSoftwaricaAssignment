4b3dc5e881b0a9c0a4475acf3f6170308b9db134
bethebikash <bhattaraibikash55@gmail.com>
2019-12-25 22:17:23 +0545

    Adding Bottom Navigation.

--- app/src/main/java/com/bhattaraibikash/esoftwaricaassignment/MainActivity.java
+++ app/src/main/java/com/bhattaraibikash/esoftwaricaassignment/MainActivity.java
@@ -1,14 +1,61 @@
 package com.bhattaraibikash.esoftwaricaassignment;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigation;
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        navigation = findViewById(R.id.bottomNavigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        toolbar.setTitle("Home");
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navHome:
+                    toolbar.setTitle("Home");
+                    return true;
+                case R.id.navAdd:
+                    toolbar.setTitle("Add Student");
+                    return true;
+                case R.id.navAbout:
+                    toolbar.setTitle("About");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameContainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
